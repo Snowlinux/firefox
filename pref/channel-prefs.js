1ead2f081383d34010d6970682db8af1dadda958
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2012-11-22 11:33:24 +0000

    Initial commit

---
+++ pref/channel-prefs.js
@@ -0,0 +1,27 @@
+//@line 2 "/home/clem/src/mozilla-release/browser/app/profile/channel-prefs.js"
+pref("app.update.channel", "default");
+
+// Use LANG environment variable to choose locale
+pref("intl.locale.matchOS", true);
+
+// Disable default browser checking.
+pref("browser.shell.checkDefaultBrowser", false);
+
+// Prevent EULA dialog to popup on first run
+pref("browser.EULA.override", true);
+
+// Default search engine
+pref("browser.search.searchEnginesURL", "http://www.linuxmint.com/searchengines/");
+
+// Activate the backspace key for browsing back
+pref("browser.backspace_action", 0);
+
+// Disable ipv6
+pref("network.dns.disableIPv6", true);
+
+// Ignore Mozilla release notes startup pages
+pref("browser.startup.homepage_override.mstone", "ignore");
+
+// Save tabs before exiting
+user_pref("browser.showQuitWarning", true);
+
---
+++ update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+ARCHI=`getconf LONG_BIT`
+if [[ $ARCHI == *64* ]]; then
+ ARCHI="amd64"
+ ARCH_URL="x86_64"
+else
+ ARCHI="i386"
+ ARCH_URL="i686"
+fi
+
+mkdir -p opt
+rm -rf opt/*
+cd opt
+wget --quiet -nd -r -l1 --no-parent -A "*.bz2" http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest/linux-$ARCH_URL/en-US/
+
+RELEASE=`ls firefox* | cut -d "-" -f2 | sed "s/.tar.bz2//"`
+bzip2 -d firefox-$RELEASE.tar.bz2
+tar xf firefox-$RELEASE.tar
+rm firefox-$RELEASE.tar
+rm firefox/omni.ja
+cd ..
+
+echo "Downloaded Firefox $RELEASE for $ARCHI"
+
+rm -rf opt/firefox/searchplugins/*
+cp searchplugins/* opt/firefox/searchplugins/
+rm -rf opt/firefox/defaults/pref/*
+cp pref/* opt/firefox/defaults/pref/
+
+rm -rf opt/robots.txt
+
+export DEBFULLNAME='Clement Lefebvre'
+export DEBEMAIL='root@linuxmint.com'
+dch -v "$RELEASE" -b
+
+dpkg-buildpackage -us -uc -rfakeroot
