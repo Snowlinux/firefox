5e0b903019834b962da5403e9a858bb5894df781
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2012-11-23 10:28:41 +0000

    Fixes

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 ARCHI=$1
 CURDIR=$2
+RELEASE=$3
 if [[ $ARCHI == *amd64* ]]; then
  ARCH_URL="x86_64"
 else
@@ -10,15 +11,13 @@ fi
 
 echo "Downloading Firefox for $ARCHI"
 cd $CURDIR/debian/firefox/opt
-wget -nd -r -l1 --no-parent -A "*.bz2" http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest/linux-$ARCH_URL/en-US/
-mv *bz2 firefox.tar.bz2
-bzip2 -d firefox.tar.bz2
-tar xvf firefox.tar
-rm firefox.tar
+wget http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest/linux-$ARCH_URL/en-US/firefox-$RELEASE.tar.bz2
+bzip2 -d firefox-$RELEASE.tar.bz2 || exit 1
+tar xvf firefox-$RELEASE.tar
+rm firefox-$RELEASE.tar
 rm firefox/omni.ja
 rm -rf firefox/searchplugins/*
 rm -rf firefox/defaults/pref/*
-rm -rf opt/robots.txt
 cp $CURDIR/searchplugins/* firefox/searchplugins/
 cp $CURDIR/pref/* firefox/defaults/pref/
 cd $CURDIR
